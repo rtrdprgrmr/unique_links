e173402719c16c555fdd4b40a7f711fe1e8b5a10
rtrdprgrmr <rtrdprgrmr@yahoo.co.jp>
2017-01-14 20:52:14 +0900

    .

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# unique_links
+# unique_links.sh bash script
+
+## Usage
+
+```sh
+unique_links.sh -add destination-directory source-file-or-directory ...
+unique_links.sh -del destination-directory source-file-or-directory ...
+```
+
+## -add
+
+"unique_links.sh -add" creates symbolic links to the specifiled files except for entirely duplicate files.
+
+For example, following command creates symbolic links under 'output' directory to plain files under ~/Backups/*/Pictures. Created symbolic links are named from the hash code of file contents. If there are entirely duplicate files under ~/Backups/*/Pictures, the corresponding symbolic link is created only once.
+
+```sh
+% mkdir output
+% unique_links.sh -add output ~/Backups/*/Pictures
+```
+
+```sh
+% ls output
+0ba1e28be7d68b19ed883e678392e6cb.JPG
+0bcc62b54d605baf0beeb606aee96e25.plist
+0bd05cfda4e1f39bf5f08b69eb66d9f9.JPG
+0c1d20edaf11631f57d5afc5d61438d9.JPG
+0c2505ad783bf25d6a4c39017328fd6c.JPG
+0c751be8bfc7fc8b447f167e53218c56.JPG
+0d1762ef6b552b0fa7109210ce41787b.db
+0e195fddfbd378eb14e39d1ccb734c9b.JPG
+0f4a0a37c6c5fbca6fdf392e8a9e87db.JPG
+0f7297ce17c9fe0eeafbce2231ea56e5.JPG
+0f932695524fb03a9260e9a5cee6a8c9.DS_Store
+0f9798c1189a68831fed96693c3a66d2.JPG
+```
+
+
+## -del
+
+"unique_links.sh -del" deletes symbolic links which has created by "unique_links.sh -add". If a symbolic link corresponds to a file which has duplicate content of one of the specifiled files, it is deleted.
+
+For example, after above example, following command deletes symbolic links under 'output' directory which are included in ~/Pictures.
+
+```sh
+% unique_links.sh -del output ~/Pictures
+```
---
+++ unique_links.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+VERBOSE=true
+#FOLLOW_SYMLINK=true
+
+usage(){
+	echo "usage: $0 -add destination-directory source-file-or-directory ..."
+	echo "usage: $0 -del destination-directory source-file-or-directory ..."
+	echo "usage: $0 -check destination-file-or-directory ..."
+}
+
+make_hash(){
+	basename=${file##*/}
+	tail=${basename##*.}
+	if [ "$tail" == "$basename" ]; then
+		ext=
+	else
+		ext=.$tail
+	fi
+	hash=$(dd if="$file" bs=4096 count=1 2>/dev/null | openssl md5)
+}
+
+make_link(){
+	n=$1
+	if [ "${n:0:1}" != "/" ]; then
+		n=$PWD/$n
+	fi
+	ln -s "$n" "$2"
+}
+
+add_action(){
+	if [ -d "$file" ]; then
+		[ $FOLLOW_SYMLINK ] && FIND_OPT=-L
+		find $FIND_OPT "$file" -type f -print0 | xargs -0 "$0" -add "$dstdir"
+		if [ $? -ne 0 ]; then
+			exit 255
+		fi
+		return
+	fi
+	if [ ! -e "$file" ]; then
+		echo "not found: $file"
+		return
+	fi
+	make_hash
+	for i in "$dstdir/$hash"*; do
+		cmp --quiet "$file" "$i"
+		if [ $? -eq 0 ]; then
+			[ $VERBOSE ] && echo "dup: $file ($i)"
+			return
+		fi
+	done
+	newfile=$dstdir/$hash$ext
+	let j=1
+	while true; do
+		if [ ! -e "$newfile" ] && [ ! -L "$newfile" ]; then
+			make_link "$file" "$newfile"
+			return
+		fi
+		newfile=$dstdir/${hash}_$j$ext
+		let j=j+1
+	done
+}
+
+del_action(){
+	if [ -d "$file" ]; then
+		[ $FOLLOW_SYMLINK ] && FIND_OPT=-L
+		find $FIND_OPT "$file" -type f -print0 | xargs -0 "$0" -del "$dstdir"
+		if [ $? -ne 0 ]; then
+			exit 255
+		fi
+		return
+	fi
+	if [ ! -e "$file" ]; then
+		echo "not found: $file"
+		return
+	fi
+	make_hash
+	for i in "$dstdir/$hash"*; do
+		cmp --quiet "$file" "$i"
+		if [ $? -eq 0 ]; then
+			[ $VERBOSE ] && echo "del: $file ($i)"
+			rm -f "$i"
+			return
+		fi
+	done
+}
+
+check_action(){
+	if [ -d "$file" ]; then
+		find "$file" -type f -print0 | xargs -0 "$0" -check
+		if [ $? -ne 0 ]; then
+			exit 255
+		fi
+		return
+	fi
+	if [ ! -e "$file" ]; then
+		echo "not found: $file"
+		return
+	fi
+	if [ ! -L "$file" ]; then
+		echo "not symlink: $file"
+		return
+	fi
+	make_hash
+	if [ "${basename%.*}" == "$hash" ]; then
+		return
+	fi
+	if [ "${basename%%_*}" == "$hash" ]; then
+		return
+	fi
+	echo "invalid: $file"
+}
+
+operation=$1
+shift
+
+if [ "$operation" == "-add" ]; then
+	dstdir=$1
+	shift
+	if [ ! -d "$dstdir" ]; then
+		echo "directory not found: $dstdir"
+		exit 255
+	fi
+	while [[ $# -gt 0 ]]; do
+		file=$1
+		add_action
+		shift
+	done
+	exit 0
+fi
+
+if [ "$operation" == "-del" ]; then
+	dstdir=$1
+	shift
+	if [ ! -d "$dstdir" ]; then
+		echo "directory not found: $dstdir"
+		exit 255
+	fi
+	while [[ $# -gt 0 ]]; do
+		file=$1
+		del_action
+		shift
+	done
+	exit 0
+fi
+
+if [ "$operation" == "-check" ]; then
+	while [[ $# -gt 0 ]]; do
+		file=$1
+		check_action
+		shift
+	done
+	exit 0
+fi
+
+usage
+exit 255
+
